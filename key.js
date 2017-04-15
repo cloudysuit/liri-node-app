4cb8768e6002302a3ca50ec296b497d98b417045
Stephen Hanson <stephenmhanson@gmail.com>
2017-04-14 23:59:32 -0500

    Delete key.js

--- key.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'TjX40n59NvYQIgdLdj6oGNLDV',
-  consumer_secret: 'TtfxC11UDZxRMGTjmSvJxD0E2wZzoQ0F7RRxbBlLVHfAxfwIwY',
-  access_token_key: '20742244-4JNBPWOhKo8aXh3JmC9baYP5lFSyEr8Qi3RAdgFMu',
-  access_token_secret: 'M0WYRcUTWomigTl1tV4Tj4axEAdFfEsyjVH2CRtgttchk',
-}
